bd5a8e0880df457ec2c5b119a51a63fe27975272
DaceM <dacemarkevica@gmail.com>
2021-09-08 17:20:46 +0300

    FizzBuzz collection

---
+++ src/main/scala/com/github/dacemarkevica/Utilities.scala
@@ -0,0 +1,10 @@
+package com.github.dacemarkevica
+
+/**
+ * Purpose: Utilities collects our commonly used utilities across the whole project
+ */
+object Utilities {
+  val MYP = 3.1415926
+  def add(a: Int, b: Int): Int = a + b // functions that are to be used by others should have full type information
+  // if we forget IntelliJ will remind at some point
+}
